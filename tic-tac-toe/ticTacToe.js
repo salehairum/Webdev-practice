fa65a748f83db4d81c1fbe39d8e2ac77468ff503
salehairum <salehairum22@gmail.com>
2024-09-08 22:16:47 +0500

    everything done(tictactoe), need to add modal for win and reset

---
+++ tic-tac-toe/ticTacToe.js
@@ -0,0 +1,93 @@
+let turn = 0;
+let spans = document.querySelectorAll('span');
+let turnText = document.querySelector('#playerNo');
+var modal = document.getElementById("winnerModal");
+var winnerMessage = document.getElementById("winnerMessage");
+var playAgainBtn = document.getElementById("playAgain");
+var closeModal = document.getElementById("closeModal");
+let noOfTurns = 0;
+
+let gameWon = 0;    //0 means continuing, 1 means player 1 win, 2 means player 2 wins, 3 means 
+
+for (let span of spans) {
+    span.addEventListener('click', function (e) {
+        if (span.textContent != 'O' && span.textContent != 'X') {
+            if (turn == 0)
+                span.textContent = 'O';
+            else span.textContent = 'X';
+            span.style.color = "#7F5539";
+            turn = !turn;
+            turnText.textContent = "Player " + (turn + 1);
+            noOfTurns++;
+            if (noOfTurns >= 5) {
+                checkWin();
+                if (gameWon == 1) {
+                    // turnText.textContent = 1;
+                    showWinner();
+                }
+                else if (gameWon == 2)
+                    turnText.textContent = 2;
+                else if (gameWon == 3)
+                    turnText.textContent = "Draw";
+            }
+        }
+    })
+}
+
+document.addEventListener('keydown', function (e) {
+    if (e.key >= 1 && e.key <= 9) {
+        if (spans[e.key - 1].textContent != 'O' && spans[e.key - 1].textContent != 'X') {
+            if (turn == 0)
+                spans[e.key - 1].textContent = 'O';
+            else spans[e.key - 1].textContent = 'X';
+            spans[e.key - 1].style.color = "#7F5539";
+            turn = !turn;
+            turnText.textContent = "Player " + (turn + 1);
+            noOfTurns++;
+            if (noOfTurns >= 5) {
+                checkWin();
+                if (gameWon == 1)
+                    turnText.textContent = 1;
+                else if (gameWon == 2)
+                    turnText.textContent = 2;
+                else if (gameWon == 3)
+                    turnText.textContent = "Draw";
+                showWinner();
+            }
+        }
+    }
+})
+
+function checkWin() {
+    if (noOfTurns == 9) {
+        gameWon = 3;
+        return;
+    }
+
+    //check each row and col
+    for (let i = 0; i < 3; i++) {
+        if ((spans[i * 3].textContent == 'O' && spans[i * 3 + 1].textContent == 'O' && spans[i * 3 + 2].textContent == 'O') || (spans[i].textContent == 'O' && spans[i + 3].textContent == 'O' && spans[i + 6].textContent == 'O')) {
+            gameWon = 1;
+            return;
+        }
+        else if ((spans[i * 3].textContent == 'X' && spans[i * 3 + 1].textContent == 'X' && spans[i * 3 + 2].textContent == 'X') || (spans[i].textContent == 'X' && spans[i + 3].textContent == 'X' && spans[i + 6].textContent == 'X')) {
+            gameWon = 2;
+            return;
+        }
+    }
+
+    //check diagonal
+    if ((spans[0].textContent == 'O' && spans[4].textContent == 'O' && spans[8].textContent == 'O') || (spans[2].textContent == 'O' && spans[4].textContent == 'O' && spans[6].textContent == 'O')) {
+        gameWon = 1;
+        return;
+    }
+    else if ((spans[0].textContent == 'X' && spans[4].textContent == 'X' && spans[8].textContent == 'X') || (spans[2].textContent == 'X' && spans[4].textContent == 'X' && spans[6].textContent == 'X')) {
+        gameWon = 2;
+        return;
+    }
+}
+
+function showWinner() {
+    winnerMessage.textContent = gameWon + " has won!";
+    modal.style.display = "block";
+}
