b0f284bc13b791a2245bd0276f41f8cdb5b2663f
Saleha Irum <117927283+salehairum@users.noreply.github.com>
2024-12-09 18:34:33 +0500

    only create new row when a full word is entered

--- wordle/wordle.js
+++ wordle/wordle.js
@@ -1,20 +1,24 @@
 let letters = document.querySelectorAll(".box");
 
-let i = 0;
+let i = 0;   //maintains the letter being filled currently
 let lettersPerRow = 5;
+let filledLetters = 0;
 let rows = 1;
 
 document.addEventListener("keydown", function (e) {
-    if (/^[a-zA-Z]$/.test(e.key)/*(e.key >= 'a' && e.key <= 'z') /*|| (e.key >= 'A' && e.key <= 'Z')*/) {
+    if (/^[a-zA-Z]$/.test(e.key)) {
         letters[i].textContent = e.key.toUpperCase();
         i++;
         if (i >= lettersPerRow)
             i = lettersPerRow - 1;
+        if (filledLetters < lettersPerRow)
+            filledLetters++;
     }
-    if (e.key === 'Enter') {
+    if (e.key === 'Enter' && filledLetters === lettersPerRow) {
         // Logic to generate a new row
         generateNewRow();
-        i = 0; // Reset i for the new row
+        i = 0; // Reset variables for the new row
+        filledLetters = 0;
     }
 })
 
