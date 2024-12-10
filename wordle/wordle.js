e06e97527a92971746a167d69aa4772f62f37f40
Saleha Irum <117927283+salehairum@users.noreply.github.com>
2024-12-10 22:07:09 +0500

    wordle: functionality of "enter"

--- wordle/wordle.js
+++ wordle/wordle.js
@@ -1,18 +1,27 @@
-let letters = document.querySelectorAll(".box");
-
-let i = 0;   //maintains the letter being filled currently
+let i = 0;   //maintains the letter being filled currently in a particular row
 let lettersPerRow = 5;
 let filledLetters = 0;
-let rows = 1;
+let currentRow = 1;
+
+let letters = document.querySelectorAll(`.row${currentRow} .box`);
 
 document.addEventListener("keydown", function (e) {
     if (/^[a-zA-Z]$/.test(e.key)) {
         letters[i].textContent = e.key.toUpperCase();
         i++;
-        if (i >= lettersPerRow)
-            i = lettersPerRow - 1;
+        if (i >= lettersPerRow) {
+            i = 0;
+        }
         if (filledLetters < lettersPerRow)
             filledLetters++;
+    }
+
+    if (e.key === "Enter") {
+        //this needs to have a lot more implementation 
+        filledLetters = 0;
+        i = 0;
+        currentRow++;
+        letters = document.querySelectorAll(`.row${currentRow} .box`);
     }
 })
 
