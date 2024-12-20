f52786c249667a08c4f7bbe1956953c2fdd09629
Saleha Irum <117927283+salehairum@users.noreply.github.com>
2024-12-20 22:08:53 +0500

    wordle:all input handling done(without word validation)

--- wordle/wordle.js
+++ wordle/wordle.js
@@ -4,60 +4,98 @@ let filledLetters = 0;
 let currentRow = 1;
 
 let letters = document.querySelectorAll(`.row${currentRow} .box`);
+let keyboard = document.querySelectorAll(`.keyboardRow .box`);  //all letter keys
+// backspace is not "text content", rather an img, so thats why i have to do this
+const backSpaceKey = document.querySelector(".fa-delete-left").parentElement;
+//enter box is styled differently, its a different class
+const enterKey = document.querySelector(".enterBox").parentElement;
+
+function assignInput(value) {
+    letters[i].classList.remove("animateInput");
+
+    // Trigger a reflow, to allow for css animation to reload
+    void letters[i].offsetWidth;
+    letters[i].classList.add("animateInput");
+    letters[i].addEventListener("animationend", () => {
+        letters[i].classList.remove("animateInput");
+    });
+
+    letters[i].setAttribute("data-taken", "true");
+
+    letters[i].textContent = value;
+    i++;
+    if (i > lettersPerRow) {
+        //if all letters are entered, then no more letters entered unless user goes back and modifies
+        i = lettersPerRow - 1;
+    }
+    if (filledLetters < lettersPerRow)
+        filledLetters++;
+
+}
+
+function enterWord() {
+    filledLetters = 0;
+    i = 0;
+    currentRow++;
+    letters = document.querySelectorAll(`.row${currentRow} .box`);
+}
+
+function backSpaceInput() {
+    if (i > 0) {
+        filledLetters--;
+        i--;
+    }
+    letters[i].textContent = ' ';
+
+    letters[i].classList.remove("animateBackSpace");
+
+    // Trigger a reflow, to allow for css animation to reload
+    void letters[i].offsetWidth;
+    letters[i].classList.add("animateBackSpace");
+    letters[i].addEventListener("animationend", () => {
+        letters[i].classList.remove("animateBackSpace");
+    });
+
+    letters[i].removeAttribute("data-taken");
+}
 
 document.addEventListener("keydown", function (e) {
     if (/^[a-zA-Z]$/.test(e.key)) {
         //for managing animations
-        letters[i].classList.remove("animateInput");
-
-        // Trigger a reflow, to allow for css animation to reload
-        void letters[i].offsetWidth;
-        letters[i].classList.add("animateInput");
-        letters[i].addEventListener("animationend", () => {
-            letters[i].classList.remove("animateInput");
-        });
-
-        letters[i].setAttribute("data-taken", "true");
-
-        letters[i].textContent = e.key.toUpperCase();
-        i++;
-        if (i > lettersPerRow) {
-            //if all letters are entered, then no more letters entered unless user goes back and modifies
-            i = lettersPerRow - 1;
-        }
-        if (filledLetters < lettersPerRow)
-            filledLetters++;
-
+        assignInput(e.key.toUpperCase());
     }
 
     if (e.key === "Enter" && filledLetters == lettersPerRow) {
         //this needs to have a lot more implementation 
-        filledLetters = 0;
-        i = 0;
-        currentRow++;
-        letters = document.querySelectorAll(`.row${currentRow} .box`);
+        enterWord();
     }
 
     if (e.key === "Backspace") {
-        if (i > 0) {
-            filledLetters--;
-            i--;
-        }
-        letters[i].textContent = ' ';
-
-        letters[i].classList.remove("animateBackSpace");
-
-        // Trigger a reflow, to allow for css animation to reload
-        void letters[i].offsetWidth;
-        letters[i].classList.add("animateBackSpace");
-        letters[i].addEventListener("animationend", () => {
-            letters[i].classList.remove("animateBackSpace");
-        });
-
-        letters[i].removeAttribute("data-taken");
+        backSpaceInput();
     }
 })
 
+for (let key of keyboard) {
+    key.addEventListener("click", function () {
+        if (key.textContent >= 'A' && key.textContent <= 'Z') {
+            assignInput(key.textContent);
+        }
+        if (key.textContent === "Enter" && filledLetters == lettersPerRow) {
+            enterWord();
+        }
+    })
+}
+
+backSpaceKey.addEventListener("click", () => {
+    backSpaceInput();
+});
+
+enterKey.addEventListener("click", () => {
+    if (filledLetters == lettersPerRow) {
+        //this needs to have a lot more implementation 
+        enterWord();
+    }
+});
 
 for (let letter of letters) {
     letter.addEventListener("click", function () {
